8314551b517aea75586cf41d44a92a79f14b5c79
ornob011 <ornob011@gmail.com>
2022-12-21 14:56:38 +0600

    Clean captions

---
+++ caption_pre_processing.py
@@ -0,0 +1,9 @@
+def text_preprocessing(data):
+    data['caption'] = data['caption'].apply(lambda x: x.lower())
+    data['caption'] = data['caption'].apply(lambda x: x.replace("[^A-Za-z]",""))
+    data['caption'] = data['caption'].apply(lambda x: x.replace("\s+"," "))
+    data['caption'] = data['caption'].apply(lambda x: " ".join([word for word in x.split() if len(word)>1]))
+    data['caption'] = "startseq "+data['caption']+" endseq"
+    return data
+    
+
